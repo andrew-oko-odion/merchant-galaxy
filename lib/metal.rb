a52fc7b8a681f0f4547d74fc5a6173fad4fc22b1
Andrew <andrew-oko-odion@gmail.com>
2019-01-31 15:32:43 +0100

    Add Metal Element

--- lib/metal.rb
+++ lib/metal.rb
@@ -6,6 +6,11 @@ class Metal
     @galaxy = galaxy
   end
 
+  def add_element(galactic_units:, name:, credits:)
+    value = credits.to_f / galaxy.value_of(galactic_units)
+
+    elements[name] = value
+  end
 
   def convert(galactic_units:, element:)
     (galaxy.value_of(galactic_units) * value_of(element)).to_i

ea2f8a4492faaebd0321a87b747de3eb6102d129
Andrew <andrew-oko-odion@gmail.com>
2019-01-31 15:57:58 +0100

    Read input from a file

---
+++ merchant_galaxy.rb
@@ -0,0 +1,21 @@
+require './lib/input_processing'
+require './lib/galaxy'
+require './lib/metal'
+
+INPUT_FILE = 'input.txt'.freeze
+
+input_file = ARGV[0] || INPUT_FILE
+
+### Initializing dependencies
+galaxy = Galaxy.new
+metal = Metal.new(galaxy)
+processor = InputProcessing.new(galaxy: galaxy, metal: metal)
+
+### Opens a file and sends line by line to the InputProcessor
+begin
+  File.open(input_file).each_line do |line|
+    processor.process_line(line)
+  end
+rescue Errno::ENOENT => e
+  puts "Invalid file! Opening '#{input_file}' returned: #{e.message}"
+end
